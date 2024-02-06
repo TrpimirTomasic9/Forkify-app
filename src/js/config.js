a27f928ee8db2a019d332f9609b46311f0ef6adc
TrpimirTomasic9 <trpimir.tomasic009@gmail.com>
2024-02-06 15:04:33 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '01ee36ac-a83a-4c36-a1d0-12331d24f9ee';
+export const MODAL_CLOSE_SEC = 2.5;
